029008e7c892b57240b6ec8b62f5be99577c47df
loc.1011 <loc.le1011@hcmut.edu.vn>
2021-08-21 16:01:49 +0700

    Alg - feat(Recursion): add sqrt() and its helper function

--- Algorithms/Recursion/recursion.cpp
+++ Algorithms/Recursion/recursion.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 using namespace std;
 
+#define abs(x) ((x >= 0) ? (x) : -(x))
+#define eps 1e-8
+
 void towerOfHanoi(int, int, int, int);
 int fib(int);
 int gcd(int, int);
 int lcm(int, int);
 int pow(int, unsigned int);
 int factorial(unsigned int);
+float findSqrtUntils(double, double, double);
+float sqrt(float);
 
 int main()
 {
     // towerOfHanoi(3, 1, 3, 2);
     // cout << fib(6);
     // cout << gcd(2, 3) << " " << lcm(2, 3);
-    cout << pow(3, 2) << " " << factorial(5);
+    // cout << pow(3, 2) << " " << factorial(5);
+    cout.precision(10);
+    cout << sqrt(2);
 }
 
 void towerOfHanoi(int n, int src, int des, int aux)
@@ -85,4 +92,27 @@ int factorial(unsigned int n)
         return 1;
     }
     return n * factorial(n - 1);
+}
+
+float findSqrtUntils(double n, double lo, double hi)
+{
+    double mid = (lo + hi) / 2;
+    double mul = mid * mid;
+    if (abs(mul - n) < eps)
+    {
+        return mid;
+    }
+    else if (mul < n)
+    {
+        return findSqrtUntils(n, mid, hi);
+    }
+    else
+    {
+        return findSqrtUntils(n, lo, mid);
+    }
+}
+
+float sqrt(float n)
+{
+    return findSqrtUntils(n, 0, n);
 }
